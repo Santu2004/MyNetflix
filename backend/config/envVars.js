25f4a23104dc6aeb61b4e6e3dffa8ac14b8d1370
Santu2004 <santoshchapadi589@gmail.com>
2026-09-13 14:42:54 +0530

    problem of uri resolved

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -3,9 +3,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const ENV_VARS = {
-	MONGO_URI: process.env.MONGODB_URI,
-	PORT: process.env.PORT || 5000,
-	JWT_SECRET: process.env.JWT_SECRET,
-	NODE_ENV: process.env.NODE_ENV,
-	TMDB_API_KEY: process.env.TMDB_API_KEY,
+    MONGODB_URI: process.env.MONGODB_URI,
+
+    PORT: process.env.PORT || 5000,
+
+    JWT_SECRET: process.env.JWT_SECRET,
+
+    NODE_ENV: process.env.NODE_ENV,
+
+    TMDB_API_KEY: process.env.TMDB_API_KEY,
 };
